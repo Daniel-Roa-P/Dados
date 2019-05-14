0fcfa79b9fc6b6055b89fcd22f7f40a3b8c81347
DanielRoa <danbron523@gmail.com>
2019-05-14 02:04:31 -0500

    añadi strings

--- Dados.hs
+++ Dados.hs
@@ -1,11 +1,19 @@
 import System.Random
  
+escribir::String->IO ()
+escribir m= do putStr m
+               putStr "\n"
+ 
 main = do
- dado1 <- randomRIO (0,5)
- dado2 <- randomRIO (0,5)
- print (comparar dado1 dado2)
-
-comparar::Int -> Int -> Int
-comparar dado1 dado2 = if dado1==dado2 then 1
-					   else if dado1>dado2 then 2
-					   else 3
+ dado1 <- randomRIO (1,6)
+ dado2 <- randomRIO (1,6)
+ print("Jugador 1")
+ print(dado1)
+ print("Jugador 2")
+ print(dado2)
+ comparar dado1 dado2
+ 
+comparar::Int -> Int -> IO ()
+comparar dado1 dado2 = if dado1==dado2 then escribir "Empate" 
+					   else if dado1>dado2 then escribir "Gano el Jugador 1" 
+					   else escribir "Gano el Jugador 2" 				   
